c6eadb6fd5edad91743a20b2e84b81b839e0b6be
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2023-01-20 19:27:50 +0800

    Entanglement test

--- v2/javascripts/index.js
+++ v2/javascripts/index.js
@@ -2,6 +2,7 @@ const game = {
     machines: [],
     entanglements: [],
     inventory: {},
+    upgCount: 0,
     machineIdCount: 0,
     lastTick: Date.now()
 }
@@ -18,6 +19,19 @@ const itemNames = [
     "ice", // air + water + aether
     "void", // steam + lava + sand + ice
 ]
+
+tiers = {
+    "water": 1,
+    "fire": 1,
+    "earth": 1,
+    "air": 1,
+    "aether": 3,
+    "steam": 2,
+    "lava": 2,
+    "sand": 2,
+    "ice": 2,
+    "void": 4
+}
 
 class Recipe {
     // Output always 1
@@ -40,7 +54,16 @@ const recipes = [
     new Recipe({"fire": 4}, "earth"),
     new Recipe({"earth": 4}, "air"),
     new Recipe({"air": 4}, "water"),
-    new Recipe({"water": 10, "fire": 10, "earth": 10, "air": 10}, "aether")
+    new Recipe({"water": 10, "fire": 10, "earth": 10, "air": 10}, "aether"),
+    new Recipe({"water": 5, "fire": 4, "aether": 1}, "steam"),
+    new Recipe({"fire": 4, "earth": 3, "aether": 1}, "lava"),
+    new Recipe({"earth": 3, "air": 2, "aether": 1}, "sand"),
+    new Recipe({"air": 2, "water": 5, "aether": 1}, "ice"),
+    new Recipe({"steam": 5, "aether": 1}, "lava"),
+    new Recipe({"lava": 5, "aether": 1}, "sand"),
+    new Recipe({"sand": 5, "aether": 1}, "ice"),
+    new Recipe({"ice": 5, "aether": 1}, "steam"),
+    new Recipe({"steam": 10, "lava": 10, "sand": 10, "ice": 10, "aether": 10}, "void"),
 ]
 
 
@@ -52,6 +75,19 @@ function insertItem(inventory, key, count) {
 }
 
 
+function getEntanglementPairing(item) {
+    for (let pair of game.entanglements) {
+        if (pair[0] === item) {
+            return pair[1]
+        }
+        if (pair[1] === item) {
+            return pair[0]
+        }
+    }
+    return item
+}
+
+
 
 class Machine {
 
@@ -60,7 +96,7 @@ class Machine {
         "Storage",
         "Extract From Inventory",
         "Transmuter",
-        "Merger"
+        "Reaction Chamber"
     ]
 
     constructor(type, id) {
@@ -102,7 +138,7 @@ class Machine {
                 let machine = game.machines.filter(m => m.id === this.targetOutput)[0]
                 // There should be only a single output slot, but I'm too lazy to include checks for that
                 for (let key of Object.keys(this.outputs)) {
-                    const maxExtractable = Math.min(this.outputs[key], dt * this.outputRate)
+                    const maxExtractable = Math.min(this.outputs[key], dt * this.outputRate * (2 ** game.upgCount))
                     insertItem(machine.inputs, key, maxExtractable)
                     this.outputs[key] -= maxExtractable
                     if (this.outputs[key] == 0) {
@@ -119,7 +155,7 @@ class Machine {
 
         switch (this.type) {
             case 0:
-                insertItem(this.outputs, this.itemType, dt * this.inputRate)
+                insertItem(this.outputs, this.itemType, dt * this.inputRate * (2 ** game.upgCount))
                 break
             case 1:
                 for (let key of Object.keys(this.inputs)) {
@@ -134,16 +170,28 @@ class Machine {
                     break
                 }
                 if (Object.keys(game.inventory).includes(this.itemType)) {
-                    const maxExtractable = Math.min(game.inventory[this.itemType], dt * this.inputRate)
+                    const maxExtractable = Math.min(game.inventory[this.itemType], dt * this.inputRate * (2 ** game.upgCount))
                     insertItem(this.outputs, this.itemType, maxExtractable)
                     game.inventory[this.itemType] -= maxExtractable
                 }
                 break
             case 3:
-                // IDK
+                const itemKeys = Object.keys(this.inputs).filter(k => this.inputs[k] >= 2)
+                if (itemKeys.length <= 0) return
+                for (let key of itemKeys) {
+                    const entangledItem = getEntanglementPairing(key)
+                    if (entangledItem !== key) {
+                        if (outputKeys.length >= 1 && !outputKeys.includes(entangledItem)) {
+                            continue
+                        }
+                        insertItem(this.outputs, entangledItem, 1)
+                        this.inputs[key] -= 2
+                        break
+                    }
+                }
                 break
             case 4:
-                // Placeholder
+                // Placeholder recipe
                 if (this.recipe.output === null) {
                     break
                 }
@@ -211,6 +259,13 @@ const app = new Vue({
             this.game.inventory[b] -= 1
             this.game.entanglements.push([a, b])
             this.selectElements = [null, null]
+        },
+        unentangle(item) {
+            const [a, b] = item
+            if ((this.game.inventory[a] || 0) < 10 || (this.game.inventory[b] || 0) < 10) return 
+            this.game.inventory[a] -= 10
+            this.game.inventory[b] -= 10
+            this.game.entanglements = this.game.entanglements.filter(x => x !== item)
         }
     },
     mounted() {
@@ -228,3 +283,4 @@ function tick() {
 
     game.lastTick = Date.now()
 }
+
